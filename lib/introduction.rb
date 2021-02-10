22c2d9f26a9f608767414ef699d2750f6032d6d6
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-10 00:08:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name}, and I am learning to prgram in #{language}."
 end
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name}, and I am learning to prgram in #{language}."
+end
 
 
 
